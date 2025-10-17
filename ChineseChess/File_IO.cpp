c3357b861fbb2e4179d4726123cf148689c8cc34
xc z <13521366337@163.com>
2025-10-18 01:29:35 +0800

    实现FileManager

--- ChineseChess/File_IO.cpp
+++ ChineseChess/File_IO.cpp
@@ -6,25 +6,63 @@
 
 using namespace std;
 using namespace FileManager_ns;
+using namespace debugger;
 
 extern debug_ex debugger_audio;
 
-void FILE_INFO::AppendLine(const std::string& content)
+void FILE_INFO::AppendLine(const std::string& _content)
 {
+	content->push_back(_content);
+	line_num++;
+	return;
 }
 
 void FILE_INFO::AppendKeyValue(const std::string& key, const std::string& value)
 {
+	content->push_back(Key_Format(key));
+	content->push_back(value);
+	line_num += 2;
+	return;
 }
 
 string FILE_INFO::GetValueByKey(const std::string& key)
 {
+	for (size_t i = 0; i < content->size(); i++)
+	{
+		if ((*content)[i] == Key_Format(key)) 
+		{
+			return (*content)[i + 1];
+		}
+	}
 	return string();
+}
+
+int FileManager_ns::FILE_INFO::GetIntValueByKey(const std::string& key)
+{
+	for (size_t i = 0; i < content->size(); i++)
+	{
+		if ((*content)[i] == Key_Format(key))
+		{
+			return atoi((*content)[i + 1].c_str());
+		}
+	}
+	return 0;
+}
+
+void FileManager_ns::FILE_INFO::Lock() const
+{
+	while (!io_complete.load());
+	return;
+}
+
+inline bool FileManager_ns::FILE_INFO::TryLock() const
+{
+	return io_complete.load();
 }
 
 string FILE_INFO::Key_Format(const std::string& key)
 {
-	return string();
+	return "[" + key + "]";
 }
 
 FileManager::FileManager()
@@ -35,7 +73,28 @@ FileManager::~FileManager()
 {
 }
 
+void FileManager::Init()
+{
+	//创建读写线程
+
+}
+
 FILE_INFO FileManager::ReadFile(const std::string& filename, const std::string& expected_md5)
+{
+	if(expected_md5.empty())
+	{
+		return ReadFile_impl(filename);
+	}
+	//verify md5
+	if (!md5_verify(filename, expected_md5))
+	{
+		return FILE_INFO(false);
+	}
+	
+	return ReadFile_impl(filename);
+}
+
+FILE_INFO FileManager_ns::FileManager::ReadFile(const std::string& filename, const bool verify)
 {
 	return FILE_INFO();
 }
@@ -73,4 +132,40 @@ bool FileManager::Certfile(const std::string& filename)
 bool FileManager::md5_verify(const std::string& filename, const std::string& expected_md5)
 {
 	return false;
+}
+
+FileManager::VERIFY_INFO FileManager::GetFileVerifyInfo(const std::string& filename)
+{
+	VERIFY_INFO result;
+	//.dat->.check
+	string verify_filename = filename.substr(0, filename.length() - 3) + ".check";
+	FILE_INFO verify_file = ReadFile_impl(verify_filename);
+	if (verify_file.valid && verify_file.line_num > 2)
+	{
+		result.private_value = verify_file.GetIntValueByKey("private_value");
+		result.md5 = verify_file.GetValueByKey("md5");
+		result.valid = 1;
+	}
+	else if (verify_file.valid && verify_file.line_num == 2)
+	{
+		result.valid = 1;
+		result.md5 = verify_file.content->at(0);
+		try
+		{
+			result.private_value = stoi(verify_file.content->at(1));
+		}
+		catch (exception e)
+		{
+			result.private_value = 0;
+			debugger_main.writelog(DWARNNING, "GetFileVerifyInfo: stoi failed for file " + verify_filename);
+			result.valid = 0;
+		}
+	}
+	else
+	{
+		result.private_value = 0;
+		result.md5 = "";
+		result.valid = 0;
+	}
+	return result;
 }
--- ChineseChess/File_IO.h
+++ ChineseChess/File_IO.h
@@ -12,20 +12,28 @@ namespace FileManager_ns
 		{
 			line_num = 0;
 			valid = 1;
+			io_complete = 0;
 			content = make_shared<vector<std::string>>();
 		}
 		FILE_INFO(bool _valid)
 		{
 			line_num = 0;
 			valid = _valid;
+			io_complete = !_valid;
 			content = make_shared<vector<std::string>>();
 		}
 		void AppendLine(const std::string& content);
 		void AppendKeyValue(const std::string& key, const std::string& value);
 		std::string GetValueByKey(const std::string& key);
+		int GetIntValueByKey(const std::string& key);
 		int line_num;
 		shared_ptr<vector<std::string>> content;
-		bool valid;
+		
+		//确保IO完成
+		void Lock() const;
+		bool TryLock() const;
+		//file exists and not empty
+		atomic<bool> valid, io_complete;
 	private:
 		std::string Key_Format(const std::string& key);
 	};
@@ -36,24 +44,30 @@ namespace FileManager_ns
 		FileManager();
 		~FileManager();
 
-
+		//sync read file, verify md5 if expected_md5 is not empty
 		FILE_INFO ReadFile(const std::string& filename, const std::string& expected_md5);
+		//async read file, verify md5 if verify is true
+		FILE_INFO ReadFile(const std::string& filename, const bool verify = 0);
 
 		void SaveConfig();
 		void ReadConfig();
 
 		void RemoveFile(const std::string& filename);
-		//void AddFile(const std::string& filename);
-		
-		
-		//void FinishFile(const std::string& filename, bool certify = 0);
+
 		void WriteFile(const std::string& filename, FILE_INFO file_content, bool certify = 0);
 		
+		void Init();
 	private:
+		struct VERIFY_INFO
+		{
+			int private_value;
+			std::string md5;
+			bool valid;
+		};
 		FILE_INFO ReadFile_impl(const std::string& filename);
 		void WriteFile_impl(const std::string& filename, FILE_INFO file_content);
 		bool Certfile(const std::string& filename);
 		bool md5_verify(const std::string& filename, const std::string& expected_md5);
-		
+		VERIFY_INFO GetFileVerifyInfo(const std::string& filename);
 	};
 }
